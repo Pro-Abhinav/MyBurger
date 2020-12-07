59ef15bd7d52226bad6ba9e96455b7c09511eba5
Pro-Abhinav <abhinav16032002@gmail.com>
2020-12-08 01:32:51 +0530

    added button restrictions

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,12 +36,35 @@ class BurgerBuilder extends Component {
 
     }
 
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount<=0){
+            return;
+        }
+        const updateCount = oldCount-1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        updateIngredients[type] = updateCount;
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - INGREDIENT_PRICES[type];
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients})
+    }
+
     render(){
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for(let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
         return (
             <Aux>
                 <Burger ingredients= {this.state.ingredients}/>
                 <BuildControls 
-                    ingredientAdded = {this.addIngredientHandler}/>
+                    ingredientAdded = {this.addIngredientHandler}
+                    ingredientRemoved = {this.removeIngredientHandler}
+                    disabled = {disabledInfo} />
             </Aux>
         )
     }

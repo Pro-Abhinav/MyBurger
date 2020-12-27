360c9bdac450c05ffa5dc5ff29cdcc6e39a6e8ab
Pro-Abhinav <abhinav16032002@gmail.com>
2020-12-28 02:41:18 +0530

    minor fixes

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -30,9 +30,9 @@ export const auth = (email, password, isSignup) => {
             returnSecureToken: true
         }
 
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyByQ7-ovRdgOipN85bxKTH8P54GMtxNVZM';
+        let url = '';
         if(!isSignup){
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyByQ7-ovRdgOipN85bxKTH8P54GMtxNVZM'
+            url = ''
         }
         axios.post(url, authData)
             .then(response => {
